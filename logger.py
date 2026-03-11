b5ddf34fc0b85706d8c5dcd24692ac09ed6e3361
SampurnaNiyogi <sampurna.niyogi06@gmail.com>
2026-03-11 14:56:47 +0530

    Changes to fix CI

--- logger.py
+++ logger.py
@@ -1,16 +1,13 @@
 import logging
-import datetime
-import time
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - [%(levelname)s] - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler("scheduler.log"),
+        logging.StreamHandler()
+    ]
+)
 
-
-logging.basicConfig(level = logging.INFO, 
-                    format = "%(asctime)s - [%(levelname)s] - %(message)s",
-                    datefmt="%Y-%m-%d %H:%M:%S",
-                    handlers=[
-                        logging.FileHandler("scheduler.log"),
-                        logging.StreamHandler()
-                    ])
-
-
-logger_object = logging.getLogger("TaskScheduler") 
+logger_object = logging.getLogger("TaskScheduler")
